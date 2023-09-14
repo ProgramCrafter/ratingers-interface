06afb93b31eabec47fd4355cf52a714032726239
Andrey <pythonhunter146@gmail.com>
2023-09-14 20:12:04 +0500

    Code macros-ization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,60 @@
 use serde::{Deserialize, Serialize};
 use eframe::{App, APP_KEY, Frame};
+use egui::{RichText, Color32, CentralPanel, TopBottomPanel, ScrollArea, menu};
+
+
+
+macro_rules! color {
+    ($r:expr, $g:expr, $b:expr) => {
+        Color32::from_rgb($r, $g, $b)
+    }
+}
+macro_rules! frame {
+    (margin: $margin:expr, fill: $r:literal $g:literal $b:literal) => {
+        egui::Frame::none().inner_margin($margin).fill(Color32::from_rgb($r, $g, $b))
+    }
+}
+macro_rules! text {
+    ($text:expr) => {
+        RichText::new($text).size(15.0)
+    };
+    ($text:expr, size $size:expr) => {
+        RichText::new($text).size($size)
+    };
+    ($text:expr, color $c:expr) => {
+        RichText::new($text).size(15.0).color($c)
+    };
+    ($text:expr, size $size:expr, color $c:expr) => {
+        RichText::new($text).size($size).color($c)
+    };
+}
+macro_rules! sized_button {
+    (to $ui:expr, size $width:expr, $height:expr, text $comment:expr) => {
+        $ui.add_sized([$width, $height], egui::Button::new($comment))
+    };
+    (to $ui:expr, size $width:expr, $height:expr, text $comment:expr, fill $c:expr) => {
+        $ui.add_sized([$width, $height], egui::Button::new($comment).fill($c))
+    };
+    (to $ui:expr, height $height:expr, $($etc:tt)+) => {
+        sized_button!(to $ui, size $ui.available_width(), $height, $($etc)+)
+    };
+}
+macro_rules! label {
+    (to $ui:expr, format ($($etc:tt)+)) => {
+        $ui.label(text!(
+          format!($($etc)+)
+        ));
+    };
+    (to $ui:expr, $comment:expr) => {
+        $ui.label($comment);
+    };
+}
+
 
 
 #[derive(Deserialize, Serialize)]
 pub struct ManyCommentsApp {
-    enable_quit_option: bool,
+    save_storage: bool,
     comments: Vec<String>,
 }
 
@@ -16,54 +66,55 @@ impl Default for ManyCommentsApp {
         }
         
         Self {
-            enable_quit_option: true,
-            comments: v
+            save_storage: false,
+            comments: v,
         }
     }
 }
 
 impl ManyCommentsApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any). `persistence` feature is enabled.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, APP_KEY).unwrap_or_default();
+        match cc.storage {
+            Some(storage) => eframe::get_value(storage, APP_KEY).unwrap_or_default(),
+            None          => Default::default()
         }
-
-        Default::default()
+    }
+    
+    fn do_save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, APP_KEY, self);
     }
 }
 
 impl App for ManyCommentsApp {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, APP_KEY, self);
+        if self.save_storage { self.do_save(storage); }
     }
     
     fn update(&mut self, ctx: &egui::Context, frame: &mut Frame) {
-        if self.enable_quit_option {
-            #[cfg(not(target_arch = "wasm32"))]   // no File->Quit on web pages!
-            egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-                egui::menu::bar(ui, |ui| {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() { frame.close(); }
-                    });
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            menu::bar(ui, |ui| {
+                ui.set_min_height(30.0);
+                ui.menu_button(text!("File"), |ui| {
+                    if ui.button("Quit").clicked() { frame.close(); }
+                    
+                    let saves_text = if self.save_storage {"Disable saves"} else {"Enable saves"};
+                    if ui.button(saves_text).clicked() {
+                        self.save_storage = !self.save_storage;
+                        self.do_save(frame.storage_mut().expect("No storage available"));
+                    }
                 });
+                ui.separator();
+                label!(to ui, format ("{} comments", self.comments.len()));
             });
-        }
+        });
         
-        egui::CentralPanel::default()
-        .frame(egui::Frame::none().inner_margin(8.0).fill(egui::Color32::from_rgb(24, 24, 24)))
-        .show(ctx, |ui| {
-            egui::ScrollArea::vertical().show_rows(ui, 68.0, self.comments.len(), |ui, row_range| {
+        CentralPanel::default().frame(frame!(margin: 8.0, fill: 24 24 24)).show(ctx, |ui| {
+            ScrollArea::vertical().show_rows(ui, 68.0, self.comments.len(), |ui, row_range| {
                 ui.vertical(|ui| {
                     for row in row_range {
-                        let comment = &self.comments[row];
-                        ui.add_sized([ui.available_width(), 60.0],
-                            egui::Button::new(egui::RichText::new(comment).size(17.0).color(egui::Color32::WHITE).weak())
-                              .fill(egui::Color32::from_rgb(48, 48, 48))
-                        );
+                        sized_button!(to ui, height 60.0,
+                          text text!(&self.comments[row], color Color32::WHITE),
+                          fill color!(48, 48, 48));
                         ui.add_space(8.0);
                     }
                 });
